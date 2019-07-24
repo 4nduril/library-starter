20d9430eed2085413aa99b9012c412642a85143a
Tobias Barth <4nduril@gmail.com>
2019-07-24 16:00:10 +0200

    Emit type declarations and allow for separate typechecking

--- src/usePropsThatChanged.ts
+++ src/usePropsThatChanged.ts
@@ -1,13 +1,18 @@
 import { useState, useRef, useEffect } from "react";
 
-export const usePropsThatChanged = <P extends {}>(props: P) => {
+export const usePropsThatChanged = <P, K extends keyof P>(
+  props: P
+): Set<string> => {
   const [propsThatChanged, setPropsThatChanged] = useState(new Set<string>());
   const prevProps = useRef<P>();
   useEffect(() => {
     if (prevProps.current) {
-      const allKeys = Object.keys({ ...prevProps.current, ...props });
-      const changed = allKeys.reduce((changed, key) => {
-        return prevProps.current[key] !== props[key]
+      const allKeys = Object.keys({
+        ...prevProps.current,
+        ...props
+      }) as Extract<K, string>[];
+      const changed = allKeys.reduce((changed, key: Extract<K, string>) => {
+        return prevProps.current && prevProps.current[key] !== props[key]
           ? changed.add(key)
           : changed;
       }, new Set<string>());
